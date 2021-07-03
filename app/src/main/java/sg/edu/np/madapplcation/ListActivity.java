d7547d70f1f96dbec0cfbd2a86de4698868de9dd
Jeng Seong Ian Wee <s10203038@connect.np.edu.sg>
2021-07-03 14:47:02 +0800

    Completed login page, moved RecyclerView to ListActivity, made MainActivity2 for registering (placeholder). Validation not done as we need the DB.

---
+++ app/src/main/java/sg/edu/np/madapplcation/ListActivity.java
@@ -0,0 +1,41 @@
+package sg.edu.np.madapplcation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        ArrayList<String> ConceptList = new ArrayList<>();
+        ConceptList.add("Introduction to Android");
+        ConceptList.add("Introduction to java");
+        ConceptList.add("Basic of activity");
+        ConceptList.add("Android Activity Lifecycle");
+        ConceptList.add("Visual and Audio");
+        ConceptList.add("Event Handling");
+        ConceptList.add("RecyclerView");
+        ConceptList.add("Designing of Mobile User Experience");
+        ConceptList.add("Data and File Storage");
+        ConceptList.add("Managing Play Store Release");
+        ConceptList.add("Introduction to Android");
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        ConceptAdaptor myAdaptor = new ConceptAdaptor(ConceptList);
+        LinearLayoutManager myLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(myLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(myAdaptor);
+    }
+
+
+}
--- app/src/main/java/sg/edu/np/madapplcation/MainActivity.java
+++ app/src/main/java/sg/edu/np/madapplcation/MainActivity.java
@@ -5,37 +5,94 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MADApp";
+    private String FILENAME = "MainActivity.java";
+
+    private TextView newUser;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> ConceptList = new ArrayList<>();
-        ConceptList.add("Introduction to Android");
-        ConceptList.add("Introduction to java");
-        ConceptList.add("Basic of activity");
-        ConceptList.add("Android Activity Lifecycle");
-        ConceptList.add("Visual and Audio");
-        ConceptList.add("Event Handling");
-        ConceptList.add("RecyclerView");
-        ConceptList.add("Designing of Mobile User Experience");
-        ConceptList.add("Data and File Storage");
-        ConceptList.add("Managing Play Store Release");
-        ConceptList.add("Introduction to Android");
+        SharedPreferences preferences = getSharedPreferences("madapp", Context.MODE_PRIVATE);
+        boolean loginSkip = preferences.getBoolean("skip", false);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        ConceptAdaptor myAdaptor = new ConceptAdaptor(ConceptList);
-        LinearLayoutManager myLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(myLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(myAdaptor);
+        CheckBox rmbBox = findViewById(R.id.checkBoxRmb);
+        rmbBox.setChecked(loginSkip);
+        rmbBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            preferences.edit().putBoolean("skip", isChecked).apply();
+            remembered();
+        });
+
+        if (loginSkip) {
+            remembered();
+        }
+
+        newUser = findViewById(R.id.textView_NewUser);
+        newUser.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionevent){
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        loginButton = findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                EditText etUsername = findViewById(R.id.editText_UserName);
+                EditText etPassword = findViewById(R.id.editText_Password);
+
+                if(isValidCredentials(etUsername.getText().toString(), etPassword.getText().toString()))
+                {
+                    Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "Valid Account", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Invalid Username / Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
+    public boolean isValidCredentials(String username, String password){
+        //soon once db is completed
+/*        if()
+        {
+            return true;
+        }*/
+
+        return false;
+    }
+
+    private void remembered() {
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
+    }
 
 }
