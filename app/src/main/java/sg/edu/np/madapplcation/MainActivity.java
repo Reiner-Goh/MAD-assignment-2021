593e849451d7d534998e52d7459766cd3ae61b8f
Jeng Seong Ian Wee <s10203038@connect.np.edu.sg>
2021-07-11 21:57:00 +0800

    created a function to insert registered user to the DB, fixed registering not adding to the DB and cleaned up some of the code.

--- app/src/main/java/sg/edu/np/madapplcation/MainActivity.java
+++ app/src/main/java/sg/edu/np/madapplcation/MainActivity.java
@@ -1,11 +1,10 @@
 package sg.edu.np.madapplcation;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -23,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     DBHelper DB;
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
